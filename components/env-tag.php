9a79b1ff98912aab8fcc6e59cbc8099e879dccc3
Daniel Bitzer <danielbitzer@me.com>
2015-11-19 11:06:23 +1030

    Env tag styling

--- components/env-tag.php
+++ components/env-tag.php
@@ -5,7 +5,7 @@ class FC_Env_Tag
 	function __construct()
 	{
 		add_action('wp_after_admin_bar_render', array($this,'js'));
-		add_action( 'wp_head', array($this,'css') );
+		add_action( 'admin_head', array($this,'css') );
 	}
 
 	function css()
@@ -14,9 +14,20 @@ class FC_Env_Tag
 		<style type="text/css">
 			#wp-admin-bar-site-name .ab-item .fc-env-tag {
 				background: #9EA3A8;
+				color: #23282D;
+				font-weight: bold;
 				font-size: 10px;
-				padding: 0 2px;
+				padding: 0 3px;
 				text-transform: uppercase;
+				margin-left: 1px;
+				border-radius: 2px;
+				position: relative;
+				top: -1px;
+			}
+
+			#wp-admin-bar-site-name:hover .ab-item .fc-env-tag,
+			#wp-admin-bar-site-name.hover .ab-item .fc-env-tag {
+				background: #00B8ED;
 			}
 		</style>
 	<?php
@@ -27,7 +38,7 @@ class FC_Env_Tag
 	?>
 		<script type="text/javascript">
 			(function($){
-				$('#wp-admin-bar-site-name .ab-item').append(' <span class="fc-env-tag"><?php echo WP_ENV ?></span>')
+				$('#wp-admin-bar-site-name > .ab-item').append(' <span class="fc-env-tag"><?php echo WP_ENV ?></span>')
 			}(jQuery));
 		</script>
 	<?php
